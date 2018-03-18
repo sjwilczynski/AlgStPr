342800db28962ef6f04164279f3a9d0042f1ccc2
sjwilczynski <sjwilczynski@gmail.com>
2018-03-18 23:49:19 +0100

    more cleanup and SGD

--- lab3/project.py
+++ lab3/project.py
@@ -1,7 +1,4 @@
-#
-# project first try
-# 
-
+# Stanisław Wilczyński project1
 import sys
 
 import matplotlib.pyplot as plt
@@ -13,7 +10,6 @@ d = 10
 
 def loss(a, x, y):
     xb = np.dot(x, a)
-    # print ('Shapes:', xb.shape, np.log(1+np.exp(xb)).shape, np.multiply(y, xb).shape)
     return np.sum(np.log(1 + np.exp(xb)) - np.multiply(y, xb))
 
 
@@ -47,18 +43,26 @@ def generate_response(X, beta, debug=False):
 
 # proximal for lambda penalty - as in the lecture
 def proximal(a, step, lam):
-    # print(a.shape, np.multiply(np.sign(a), np.maximum((np.abs(a) -  lam*step), np.zeros((d,1)))).shape)
     return np.multiply(np.sign(a), np.maximum((np.abs(a) - lam * step), np.zeros((d, 1))))
 
 
-# gradient for logistic regression model - as in project
+# gradient step for logistic regression model - as in project
 def grad_step(a, x, y, step=0.01):
     p = np.exp(np.dot(x, a)) / (1 + np.exp(np.dot(x, a))).reshape(n, 1)
     grad_a = np.dot(x.T, (p - y))
     return a - step * grad_a
 
 
-def gradient_descent(x, y, epsilon=0.0000000001, step=0.01, lam=0.01, prox=False):
+# stochastic gradient step - calculate gradient with respect only to loss connected with randomly chosen x_j
+def stochastic_grad_step(a, x, y, step=0.01):
+    j = np.random.randint(n)
+    p = np.exp(np.dot(x[j, :], a)) / (1 + np.exp(np.dot(x[j, :], a))).ravel()[0]
+    grad_a = (p - y[j]) * x[j, :]
+    return a - step * grad_a.reshape(-1, 1)
+
+
+# gradient descent algorithm
+def gradient_descent(x, y, epsilon=0.0000000001, step=0.01, lam=0.01, prox=False, stochastic=False):
     a = np.random.randn(d).reshape(d, 1)
     losses = []
     prev_loss = sys.maxsize
@@ -66,7 +70,10 @@ def gradient_descent(x, y, epsilon=0.0000000001, step=0.01, lam=0.01, prox=False
     steps = 0
     while np.abs(curr_loss - prev_loss) > epsilon and steps < 10 ** 4:
         losses += [loss(a, x, y)]
-        a = grad_step(a, x, y, step)
+        if stochastic:
+            a = stochastic_grad_step(a, x, y, step)
+        else:
+            a = grad_step(a, x, y, step)
         if prox:
             a = proximal(a, step, lam)
         steps += 1
@@ -75,6 +82,7 @@ def gradient_descent(x, y, epsilon=0.0000000001, step=0.01, lam=0.01, prox=False
     return a, losses, steps
 
 
+# saga algorithm
 def saga(x, y, epsilon=0.00000001, step=0.01, prox=False):
     a = np.zeros(d).reshape(d, 1)
     p = np.exp(np.dot(x, a)) / (1 + np.exp(np.dot(x, a))).reshape(n, 1)
@@ -103,16 +111,21 @@ def saga(x, y, epsilon=0.00000001, step=0.01, prox=False):
     return a, losses, steps
 
 
+# svrg algorithm
+def svrg(x, y, epsilon=0.00001, step=0.01, lam=0.01, prox=False):
+    pass
+
+
 beta = np.random.uniform(-1, 2, d).reshape(-1, 1)  # very important reshape
 
 ###mostly zeros beta
-#ind = np.random.permutation(np.arange(d))[:-d//8]
-#beta[ind] = 0
+# ind = np.random.permutation(np.arange(d))[:-d//8]
+# beta[ind] = 0
 ####
 x = generate_data(n, d, 0.9, 'independent')
 y = generate_response(x, beta)
 
-start_ind = 0
+start_ind = 2
 a, losses, steps_count = gradient_descent(x, y)
 print('Number of nonzero coeff: {}'.format(np.sum(a != 0)))
 print('Squared error for betas: {}'.format(np.sum((a - beta) ** 2)))
@@ -120,7 +133,14 @@ print('Steps count for GD: {}\nThe loss was {}'.format(steps_count, losses[-1]))
 plt.figure(1)
 plt.plot(np.arange(len(losses[start_ind:])), losses[start_ind:], label='Loss for GD')
 
-a, losses, steps_count = saga(x, y, step=0.001)
+a, losses, steps_count = gradient_descent(x, y, step=0.02, stochastic=True)
+print('Number of nonzero coeff: {}'.format(np.sum(a != 0)))
+print('Squared error for betas: {}'.format(np.sum((a - beta) ** 2)))
+print('Steps count for SGD: {}\nThe loss was {}'.format(steps_count, losses[-1]))
+plt.figure(1)
+plt.plot(np.arange(len(losses[start_ind:])), losses[start_ind:], label='Loss for SGD')
+
+a, losses, steps_count = saga(x, y, step=0.05)
 print('Number of nonzero coeff: {}'.format(np.sum(a != 0)))
 print('Squared error for betas: {}'.format(np.sum((a - beta) ** 2)))
 print('Steps count for SAGA: {}\nThe loss was {}'.format(steps_count, losses[-1]))
